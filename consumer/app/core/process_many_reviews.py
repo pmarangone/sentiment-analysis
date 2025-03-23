3e8ff37965596de014666c1dc611c00a20bd703a
pmarangone <patrickmarangone@gmail.com>
2025-03-23 14:46:31 -0300

    refactor(consumer): improved project readability

---
+++ consumer/app/core/process_many_reviews.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+import base64
+import json
+
+from app.ml_models.sentiment_analysis import analyzer
+from app.utils.logger import get_logger
+from app.db.review_repository import review_repository
+
+logger = get_logger(__name__)
+
+
+def process_many_reviews(message):
+    logger.info("Processing multiple reviews")
+    data = json.loads(message)
+    today = datetime.today().strftime("%Y-%m-%d")
+
+    reviews_to_update = []
+
+    for message_data in data:
+        review_id = message_data["review_id"]
+        review_bytes = message_data["review_bytes"]
+        review_sentence = base64.b64decode(review_bytes).decode("utf-8")
+
+        prediction = analyzer.predict(review_sentence)
+        logger.info(f"Prediction for review {review_id}: {prediction}")
+
+        review_update = {
+            "id": review_id,
+            "classification": prediction.output,
+            "sentiment_scores": {
+                "positive": round(prediction.probas["POS"], 3),
+                "negative": round(prediction.probas["NEG"], 3),
+                "neutral": round(prediction.probas["NEU"], 3),
+            },
+            "classified_at": today,
+            "classified": True,
+        }
+        reviews_to_update.append(review_update)
+
+    if reviews_to_update:
+        Session = review_repository.sessionmaker
+        session = Session()
+
+        try:
+            review_repository.bulk_update_reviews(session, reviews_to_update)
+            session.commit()
+            logger.info(f"Bulk update completed for {len(reviews_to_update)} reviews.")
+        finally:
+            session.close()
---
+++ consumer/app/core/process_single_review.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+import base64
+import json
+
+from app.ml_models.sentiment_analysis import analyzer
+from app.utils.logger import get_logger
+from app.db.review_repository import review_repository
+
+logger = get_logger(__name__)
+
+
+def process_review(message):
+    message_data = json.loads(message)
+    review_id = message_data["review_id"]
+    review_bytes = message_data["review_bytes"]
+    review_sentence = base64.b64decode(review_bytes).decode("utf-8")
+
+    today = datetime.today().strftime("%Y-%m-%d")
+
+    prediction = analyzer.predict(review_sentence)
+
+    logger.info(f"prediction: {prediction}")
+
+    with review_repository.sessionmaker() as session:
+        review = review_repository.update_review(session, review_id)
+
+        if review:
+            match prediction.output:
+                case "POS":
+                    review.classification = "positive"
+                case "NEG":
+                    review.classification = "negative"
+                case "NEU":
+                    review.classification = "neutral"
+
+            review.sentiment_scores = {
+                "positive": round(prediction.probas["POS"], 3),
+                "negative": round(prediction.probas["NEG"], 3),
+                "neutral": round(prediction.probas["NEU"], 3),
+            }
+            review.classified_at = today
+            review.classified = True
+            session.commit()
+
+        else:
+            logger.warning(f"Review with id {review_id} not found")
+
+    logger.info(f"Prediction for id {review_id} completed successfully.")
